a7560bf076fe66399684e0b91a87f84756eb6104
Jakub M <k.miranowski@gmail.com>
2023-03-27 12:27:58 +0200

    trapeze

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -24,7 +24,20 @@ field_c = numpy.pi * (r_c ** 2)
 
 output_c = f'Perimeter of the circle is equal to {perimeter_c} and field is equal to {field_c}.'
 
+#trapeze
+
+a_t = 3
+b_t = 28
+c_t = 10
+d_t = 15
+h_t = 12
+
+perimeter_t = a_t + b_t + c_t + d_t
+field_t = ((a_t + b_t) * h_t)/2
+
+output_t = f'Perimeter of the trapeze is equal to {perimeter_t} and field is equal to {field_t}.'
+
 print(output_s)
 print(output_r)
 print(output_c)
-
+print(output_t)
