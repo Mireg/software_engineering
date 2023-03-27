506b04e4e4a0d946a5c99b4c76a680c7e836cada
Jakub M <k.miranowski@gmail.com>
2023-03-27 12:16:02 +0200

    rectangle

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,9 +1,19 @@
 # square
 
-a = 15
-perimeter = a * 4
-field = a ** 2
+a_s = 15
+perimeter_s = a_s * 4
+field_s = a_s ** 2
 
-output = f'Perimeter of the square is equeal to {perimeter} and field is equal to {field}.'
+output_s = f'Perimeter of the square is equal to {perimeter_s} and field is equal to {field_s}.'
 
-print(output)
+#rectangle
+
+a_r = 10
+b_r = 8
+perimeter_r = 2 * a_r + 2 * b_r
+field_r =  a_r * b_r
+
+output_r = f'Perimeter of the rectangle is equal to {perimeter_r} and field is equal to {field_r}.'
+
+print(output_s)
+print(output_r)
