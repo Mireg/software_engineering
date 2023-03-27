267e57c52191af439c15ecd6ace09bb89f0aa563
Jakub M <k.miranowski@gmail.com>
2023-03-27 12:32:16 +0200

    RHOMBUS

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,4 +1,5 @@
 import numpy
+
 # square
 
 a_s = 15
@@ -7,16 +8,16 @@ field_s = a_s ** 2
 
 output_s = f'Perimeter of the square is equal to {perimeter_s} and field is equal to {field_s}.'
 
-#rectangle
+# rectangle
 
 a_r = 10
 b_r = 8
 perimeter_r = 2 * a_r + 2 * b_r
-field_r =  a_r * b_r
+field_r = a_r * b_r
 
 output_r = f'Perimeter of the rectangle is equal to {perimeter_r} and field is equal to {field_r}.'
 
-#circle
+# circle
 
 r_c = 7
 perimeter_c = 2 * numpy.pi * r_c
@@ -24,7 +25,7 @@ field_c = numpy.pi * (r_c ** 2)
 
 output_c = f'Perimeter of the circle is equal to {perimeter_c} and field is equal to {field_c}.'
 
-#trapeze
+# trapeze
 
 a_t = 3
 b_t = 28
@@ -33,11 +34,22 @@ d_t = 15
 h_t = 12
 
 perimeter_t = a_t + b_t + c_t + d_t
-field_t = ((a_t + b_t) * h_t)/2
+field_t = ((a_t + b_t) * h_t) / 2
 
 output_t = f'Perimeter of the trapeze is equal to {perimeter_t} and field is equal to {field_t}.'
+
+# rhombus
+
+a_rh = 9
+h_rh = 13
+
+perimeter_rh = a_rh * 4
+field_rh = a_rh * h_rh
+
+output_t = f'Perimeter of the rhombus is equal to {perimeter_rh} and field is equal to {field_rh}.'
 
 print(output_s)
 print(output_r)
 print(output_c)
 print(output_t)
+print(output_rh)
