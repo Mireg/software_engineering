547ad516a536a2c13dbed8a3d982dc872fdc0208
Jakub M <k.miranowski@gmail.com>
2023-04-13 09:28:14 +0200

    zadanie 1.8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -72,3 +72,21 @@ studenci.sort()
 print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
+
+# zadanie 1.8
+
+# posortuj alfabetycznie (od nazwiska) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
+def nazwiska(imie):
+    return imie.split(" ")[-1].lower()
+
+studenci.sort(key=nazwiska)
+# oczekiwany rezultat:
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+# Anna Szczesny
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
