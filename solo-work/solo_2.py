45354c519f9078a67b1e307fb1c3f246e9268b31
Jakub M <k.miranowski@gmail.com>
2023-05-15 12:20:46 +0200

    moj kwadrat

--- solo-work/solo_2.py
+++ solo-work/solo_2.py
@@ -8,8 +8,31 @@ class Trojkat:
     
     def obwod(self):
         return(self.a + self.b + self.c)
+    
+    
+    def pole(self):
+        return((self.a * self.h_a) / 2)
 
+class Kwadrat:
+    def __init__(self, a):
+        self.a = a
+
+    
+    def obwod(self):
+        return(4 * self.a)
+    
+    def pole(self):
+        return(pow(self.a))
+    
 
 trojkat_rownoboczny = Trojkat(10, 10, 10, 8)
-
+moj_trojkat = Trojkat(6, 6, 6, 5)
 print(trojkat_rownoboczny.obwod())
+print(moj_trojkat.obwod())
+print(f'Mój trójkąt obwód: {moj_trojkat.obwod()}')
+print(f'Mój trójkąt pole: {moj_trojkat.pole()}')
+
+
+moj_kwadrat = Kwadrat(17)
+print(f'Mój kwadrat obwód: {moj_kwadrat.obwod()}')
+print(f'Mój kwadrat pole: {moj_kwadrat.pole()}')
