3970628ccaecae4ada420c34bca09151431dbcc0
Jakub M <k.miranowski@gmail.com>
2023-06-01 23:46:23 +0200

    factorial

--- solo-work/solo_reccursion.py
+++ solo-work/solo_reccursion.py
@@ -15,7 +15,7 @@
 #         return list[0] + sumList(list[1:])
 
 list0 = []
-list1 = [1, 1, 1, 1]
+list1 = [1, 1, 1, 2]
 
 # print(sumList(list0))
 # print(sumList(list1))
@@ -31,18 +31,42 @@ list1 = [1, 1, 1, 1]
 #      else:
 #          return find_max(list.pop(1)) 
 
-def find_max(list):
-    if len(list) == 0:
-        return 0
-    elif len(list) == 1:
-        return list[0]
-    elif len(list) > 1:
-        if list[0] <= list[1]:
-            return find_max(list[1:])
-        else:
-            list.pop(1)
-            return find_max(list)
+# def find_max(list):
+#     if len(list) == 0:
+#         return 0
+#     elif len(list) == 1:
+#         return list[0]
+#     elif len(list) > 1:
+#         if list[0] <= list[1]:
+#             return find_max(list[1:])
+#         else:
+#             list.pop(1)
+#             return find_max(list)
 
-list2 = [1, 2, 2, 5, 4, 3]
-print(find_max(list2))
-print(find_max(list0))
+
+# list2 = [1, 2, 2, 5, 4, 3]
+# print(find_max(list2))
+# print(find_max(list0))
+# print(find_max(list1))
+
+
+#Factorial
+
+#funkcja factorial(n)
+#   if n == 0
+#       return 1
+#   else
+#       f = n * factorial(n-1)
+#       return f
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(5))
+
