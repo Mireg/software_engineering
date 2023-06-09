5ca331850d2ded026139bb328c5f151d75556e23
Jakub M <k.miranowski@gmail.com>
2023-06-09 23:34:27 +0200

    sudoku 4x4

--- solo-work/solo_reccursion.py
+++ solo-work/solo_reccursion.py
@@ -90,5 +90,72 @@ def F(n):
     else:
         return F(n-1) + F(n-2)
 
+#Sudoku 4x4
 
-print(f"{F(0)}, {F(1)}, {F(2)}, {F(3)}, {F(4)}, {F(5)}, ... , {F(12)}")
+#separate function?
+#check for empty cell = 0
+#check every number 1-4 if can be placed
+    #check if can be placed in row
+    #check if can be placed in column
+    #check if can be placed in 2x2 square
+
+#iterate over every row, 
+    #iterate over every column
+        #check if empty
+            #if empty check every number 1-4
+                #put the first one that works
+                    #solve new sudoku with inputed number
+                    #if cannot be solved change it back to 0
+                    #and check other numbers left in range
+
+import numpy as np
+
+sudoku = [[1,0,0,0],
+          [0,0,0,4],
+          [0,0,2,0],
+          [0,3,0,0]]
+
+def is_possible (matrix, row, col, n):
+    #check row
+    for i in range(0,4):
+        if matrix[row][i] == n:
+            return False
+    
+    #check column
+    for i in range(0,4):
+        if matrix[i][col] == n:
+            return False
+
+    #check 2x2 square
+    row0 = (row // 2) * 2
+    col0 = (col // 2) * 2
+    for i in range(2):
+        for j in range(2):
+            if matrix[row0 + i][col0 + j] == n:
+                return False
+    
+    return True
+
+print(is_possible(sudoku, 0, 2, 3))
+
+def solve_sudoku(matrix):
+    #iterate rows
+    for i in range(4):
+        #iterate columns
+        for j in range(4):
+            #check empty
+            if matrix[i][j] == 0:
+                #input all values from range
+                for n in range(1, 5):
+                    #check if it works
+                    if is_possible(matrix, i, j, n):
+                        #input number
+                        matrix[i][j] = n
+                        #solve filled sudoku
+                        solve_sudoku(matrix)
+                        #backtrack if cannot be solved
+                        matrix[i][j] = 0
+                return None
+    print(np.matrix(matrix))
+
+solve_sudoku(sudoku)
